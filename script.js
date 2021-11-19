02cc671128c3c210942f4d457f5327f194a91591
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2021-11-19 01:52:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// var x='1';     correct
+// console.log(x)
+
+// console.log(x) correct but undefined
+// var x='1'
+
+ 
+
+// here errors occured wrong
+console.log(x)
+ let x='10';        
+//  console.log(x)
+
+
+
+
+
+
+
+// console.log(a) wrong errors occured
+// const a='kk';
